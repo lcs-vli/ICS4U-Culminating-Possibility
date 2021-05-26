c89d0fa6e0e84f2dbd353e33ebc4a95b52072010
Victoria Li <VLi@lcs.on.ca>
2021-05-26 19:37:52 -0400

    the tree moves in a way that the left branch and the right branch move further or closer

--- Animation/LindenmayerSystemSketch.swift
+++ Animation/LindenmayerSystemSketch.swift
@@ -7,6 +7,11 @@ class LindenmayerSystemSketch: NSObject, Sketchable {
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     var canvas: Canvas
+    var system: LindenmayerSystem
+    var visualizeSystem: Visualizer
+    
+    var perlinNoise = PerlinGenerator()
+    var step = 0.1
     
     // This function runs once
     override init() {
@@ -17,26 +22,32 @@ class LindenmayerSystemSketch: NSObject, Sketchable {
         // Enable faster rendering
         canvas.highPerformance = true
         
+        //let randomValue = Int.random(in: 1...45)
+        //let randomPerlinValue = perlinNoise.perlinNoise(x: Double(canvas.frameCount) * step)
+        //let angle = map(value: randomPerlinValue,
+                                 //fromLower: 0,
+                                 //fromUpper: 1.0,
+                                 //toLower: 0,
+                                 //toUpper: 45)
         
         //define the L system
-        var system = LindenmayerSystem(axiom: "0XXF",
+        system = LindenmayerSystem(axiom: "0XXF",
                                        rules: [
                                         "F": [
                                             Successor(odds: 1, text: "J[-J][++J]"),
                                         ],
                                         "J": [
-                                            Successor(odds: 1, text: "XJ[-J][++J]"),
-                                            Successor(odds: 1, text: "XJ[--J][+J]"),
+                                            Successor(odds: 1, text: "XJ[-J][+J]"),
                                         ],
                                        ],
                                        generations: 5)
         
         //Visualize the system
-        var visualizeSystem  = Visualizer(for: system,
+        visualizeSystem  = Visualizer(for: system,
                                           on: canvas,
                                           length: 50,
                                           reduction: 1.25,
-                                          angle: 15.5,
+                                          angle: 45,
                                           initialPosition: Point(x: 250, y: 50),
                                           initialHeading: 90,
                                           radius: 20)
@@ -59,7 +70,27 @@ class LindenmayerSystemSketch: NSObject, Sketchable {
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
-        // Nothing is being animated, so nothing needed here
+        let randomValue = Int.random(in: 1...45)
+        
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(at: Point(x: 0, y: 0), width: 500, height: 500)
+        
+        let randomPerlinValue = perlinNoise.perlinNoise(x: Double(canvas.frameCount) * step)
+        let angle = map(value: randomPerlinValue,
+                                 fromLower: 0,
+                                 fromUpper: 1.0,
+                                 toLower: 0,
+                                 toUpper: 45)
+        //Visualize the system
+        visualizeSystem  = Visualizer(for: system,
+                                          on: canvas,
+                                          length: 50,
+                                          reduction: 1.25,
+                                          angle: Degrees(Int(angle)),
+                                          initialPosition: Point(x: 250, y: 50),
+                                          initialHeading: 90,
+                                          radius: 40)
+        visualizeSystem.render()
         
     }
     
